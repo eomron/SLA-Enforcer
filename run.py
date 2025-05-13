002e4c19535eeb871b1d89e7c463cae6e0ccae49
Emmett Omron <emomron2025@loyolablakefield.org>
2025-05-13 09:42:06 -0400

    change structuring of __init__, write docstrings

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from concurrent.futures import ThreadPoolExecutor
 from scorer import ScoreChecker
-numChecks = 10
 
+def main():
+    checker = ScoreChecker("test","172.253.115.101",443)
+    print("\"" + str(checker.url("https")[0]) + "\"")
 
-def runTests(scorers: list):
+def runTests(scorers: list,numChecks:int):
     results = []
     with ThreadPoolExecutor(max_workers=numChecks) as pool:
         futures = {pool.submit(scorer.test): scorer for scorer in scorers}
@@ -16,7 +18,7 @@ def runTests(scorers: list):
                 results.append((scorer.name,scorer.ip,scorer.type,e))
     return results
 
-checker = ScoreChecker("test","172.253.115.101",443)
-print(checker.url("https"))
+if __name__ == "__main__":
+    main()
 
 # a new ScoreChecker object should be instantiated for every vuln in the config CSV, stored as a list
--- scorer.py
+++ scorer.py
@@ -3,9 +3,26 @@ import socket
 import requests
 import struct
 import subprocess
+import sys
+import os
+
+if __name__ == "__main__" and os.path.basename(sys.argv[0]) != "run.py": #if we run this file by accident, it executes run.py anyway
+    import run
+    run.main()
+    sys.exit()
 
 class ScoreChecker:
-    def __init__(self,name: str,ip: str,port: int,*args):
+    def __init__(self,name: str,ip: str,port: int,type: str,*args):
+        """
+        Initializes the information about the check; validates the IP address.
+
+        Args:
+            str: The name of this check.
+            str: The IP address of the target.
+            int: The port of the service to check on the target.
+            str: The type of check. Valid types are listed in the documentation.
+            tuple: Any further arguments, depending on the type of check.
+        """
         self.name: str = name
         try:
             ipaddress.ip_address(ip)
@@ -13,31 +30,93 @@ class ScoreChecker:
             raise ValueError(f"Error on ScoreChecker object {self.name}: Invalid IP address {ip}. Format as a standard IPv4 address. Do not use CIDR postfixes (i.e. /24, /17).")
         self.ip: str = ip
         self.port: int = port
-        self.type: str = "ping" if len(args) == 0 else args[0] #when calling the arguments list, remember that args[0] is the type of check
-        arguments = args
+        self.type: str = type
+        self.arguments = args
 
 
-    def url(self,protocol: str): #several checks call the target as a webpage; this method fetches the data.
-        protocol = protocol.lower()
-        if protocol != "http" and protocol != "https":
+    def url(self,*args) -> tuple:
+        """
+        Calls the target as a webpage.
+
+        Args:
+            str: The path to the desired page, relative to the IP address. For example, "auth/login/success".
+            str: Either "https" or "http". Must be specified if a non-default port (not 80 or 443) is being used.
+            tuple: This method takes any number of arguments, but further arguments are ignored.
+
+        Returns:
+            tuple: A tuple containing an int (the HTTP status code of the page) and a str (the data from the response).
+
+        """
+        protocol: str = ""
+        if args[1] == "https" or self.port == 443:
+            protocol = "https"
+        elif args[1] == "http" or self.port == 80:
+            protocol = "http"
+        else:
             raise ValueError(f"Error on ScoreChecker object {self.name}: Invalid protocol (should be http or https)")
-        response = requests.get(protocol + "://" + self.ip + ":" + str(self.port),verify=False) #this ignores SSL by design because most training scenarios are going to be self-signed.
-        return [response.status_code,response.text]
+        protocol = protocol.lower()
+        page: str = args[0] if len(args) > 0 else ""
+        page = page.strip("/")
+        response = requests.get(protocol + "://" + self.ip + ":" + str(self.port) + "/" + page,verify=False) #this ignores SSL by design because most training scenarios are going to be self-signed.
+        return (response.status_code,response.text)
 
-    def ping(self):
+    def ping(self,*args) -> bool: #TODO: implement support for UDP 
+        '''
+        Attempts to connect to the target on the specified port.
+
+        Args:
+            tuple: This method takes any number of arguments, but all are ignored.
+
+        Returns:
+            bool: Whether or not the attempt to connect was successful.
+        '''
         try:
             with socket.create_connection((self.ip,self.port),timeout=4):
                 return(True)
         except(socket.timeout, ConnectionRefusedError, OSError):
                 return(False)
 
-    def httpcontent(self):
-        pass
+    def content(self,*args) -> bool:
+        '''
+        Checks that the target webpage contains a specific string.
 
-    def index(self):
-        pass
+        Args:
+            str: The path to the desired page, relative to the IP address. For example, "auth/login/success".
+            str: Either "https" or "http". Must be specified if a non-default port (not 80 or 443) is being used.
+            str: The desired string to be found in the webpage.
+            tuple: This method takes any number of arguments, but further arguments are ignored.
 
-    def dns(self):
+        Returns:
+            bool: Whether or not the content was found.
+        '''
+        if len(args) > 2:
+            return(args[2] in self.url(args)[1])
+        else:
+            raise TypeError(f"Error on ScoreChecker object {self.name}: Type is declared as \"content\", but not enough arguments are supplied (requires at least 3).")
+
+    def pageExists(self,*args) -> bool:
+        #for this function, the line should be:
+        #name,ip,port,pageExists,<webpage>,<protocol> (remove this comment when i write documentation for config.csv)
+        """
+        Checks whether a webpage responds with a specific status code, default is 200.
+        
+        Args:
+            str: The path to the desired page, relative to the IP address. For example, "auth/login/success". Set as an empty string if you wish to access the raw IP address.
+            str: Either "https" or "http". Must be specified if a non-default port (not 80 or 443) is being used.
+            int: The desired status code.
+            tuple: This method takes any number of arguments, but further arguments are ignored.
+
+        Returns:
+            bool: Whether the specific status code was received.
+        """
+        statusCode = args[2] if len(args) > 2 else 200
+        return self.url(args)[0] == statusCode
+
+    def dns(self,*args) -> bool:
+        """
+        
+        
+        """
         pass
 
     def smb(self):
@@ -48,28 +127,10 @@ class ScoreChecker:
         methods = {
             "ping": self.ping,
             "content": self.content,
-            "index": self.index,
+            "pageExists": self.pageExists,
             "dns": self.dns,
             "smb": self.smb
         }
         if self.type not in methods:
             raise ValueError(f"Unknown test type \"{self.type}\"")
-        return methods[self.type](self.arguments)
-
-
-
-        #with ThreadPoolExecutor(10) as pool: #Not sure where i go from here
-
-        #if self.type == "ping": #check if a port is listening. TODO: implement support for UDP 
-        #    pass #link it up later
-
-
-#        if self.type == "content": #check for a certain string in the HTTP/HTTPS response
-#            pass
-#        if self.type == "index": #check that <ip>/index.html responds with 200 OK. good for testing if a website is up
-#            pass
-#        if self.type == "dns": #check that the target (a DNS server) responds with a specific record
-#            pass
-#        if self.type == "smb": #check that a SMB share on the target is accessible
-#            pass
-
+        return methods[self.type](self.arguments) #because of how this is written, every method listed here must take a tuple in its arguments
